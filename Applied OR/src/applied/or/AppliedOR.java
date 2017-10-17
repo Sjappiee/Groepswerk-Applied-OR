82536ce57cdf99acce3dfe6e6056b5d223e05198
stephan <stephan@172.17.254.224>
2017-10-17 10:40:29 +0200

    test

--- Applied OR/src/applied/or/AppliedOR.java
+++ Applied OR/src/applied/or/AppliedOR.java
@@ -17,6 +17,8 @@ public class AppliedOR {
     public static void main(String[] args) {
         System.out.println("Hello fgt");
         System.out.println("SOnck kan het nie");
+        
+        System.out.println("Het lukt!");
     }
     
 }
