5c59b026292d755b2f09cad249f790c11c037d52
stephan <stephan@172.17.63.95>
2017-10-27 01:04:45 +0200

    inlezen

--- Applied OR/src/applied/or/AppliedOR.java
+++ Applied OR/src/applied/or/AppliedOR.java
@@ -16,13 +16,25 @@ import java.util.Scanner;
  */
 public class AppliedOR {
  
-    public static void main(String[] args) {
-        Process process = new Process();
+    public static void main(String[] args) throws IOException {
+
+    /*    Process process = new Process();
        Department depA = new Department ('A',32);
-       process.read_personnel_characteristics('A');
+       process.read_personnel_characteristics('A',32);
        int x;
        for(x=0;x<depA.getAmountNurses();x++){
        System.out.println(process.personelNr[x] + '\n');
-       }  
+       }*/
+       
+       ExcellReader test = new ExcellReader ();
+       test.setInputFile("C:\\Users\\stephan\\Documents\\NetBeansProjects\\Groepswerk-Applied-OR\\EchteTest.xls");
+       
+       test.giveNurseNumber(31);
+       test.giveBinaryDayPlanning(31);
+       test.giveEmploymentRate(31);
+       test.giveType(31);
+       test.givePref(31);
+        }
+
     } 
-}
+
