df4efdbb01b30efa4fc1b518f65ce5888bf39d88
stephan <stephan@172.17.63.95>
2017-10-27 11:33:45 +0200

    inlezen

--- Applied OR/src/applied/or/Nurse.java
+++ Applied OR/src/applied/or/Nurse.java
@@ -1,11 +1,28 @@
 
 package applied.or;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Nurse {
     
     private int nr;
+    private String preference;
     private int type;
     private float employmentRate;
-   
+    private int [] [] binaryDayPlanning; 
+
+    public Nurse(int nr, String preference, int type, float employmentRate) {
+        this.nr = nr;
+        this.preference = preference;
+        this.type = type;
+        this.employmentRate = employmentRate;
+        binaryDayPlanning = new int [7] [];//elke dag 2 opties
+    }
+
+    public Nurse() {
+    }
     
+    
+   
 }
